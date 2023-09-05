d8f89f2995f65bdc753d8cb90fb0c5f03dd85ba4
huangshun <1692470435@qq.com>
2023-09-06 07:36:43 +0800

    led点阵屏动画

--- 9-1 LED点阵屏显示图形/main.c
+++ 9-1 LED点阵屏显示图形/main.c
@@ -1,12 +1,19 @@
 #include <REGX52.H>
-
+#include "delay.h"
+#define matrix_led_port P0
 sbit SER=P3^4;	
 sbit SCK=P3^6;	//SRCLK
 sbit RCK=P3^5;	//RCLK
 
+/**
+  * @brief  74HC595写入一个字节
+  * @param  要写入的字节
+  * @retval 无
+  */
 void _74HC595_writebyte(unsigned char byte)
 {
 	unsigned char i;
+	//P2=~byte;
 	for(i=0;i<8;i++)
 	{
 		SER=byte&(0x80>>i);//位赋值，大于0则赋1，等于0赋0
@@ -15,16 +22,36 @@ void _74HC595_writebyte(unsigned char byte)
 	}
 	RCK=1;
 	RCK=0;
+
 } 
+
+/**
+  * @brief  LED点阵屏选择一列显示数据
+  * @param  column：选择的列；范围：0-7；0在最左边 
+  * @param  da：选择列显示的数据，高位在上，1为亮，0为灭
+  * @retval 无
+  */
+void matrixled_showcolumn(unsigned char column,da)
+{
+	_74HC595_writebyte(da);
+	matrix_led_port=~(0x80>>column);//第column列为低电平
+	delay(1);
+	matrix_led_port=0xff;//位清零，防残影
+}
 
 void main()
 {
-	P0=0;
 	SCK=0;
 	RCK=0;
-	_74HC595_writebyte(0xaa);
 	while(1)
 	{
-		
+		matrixled_showcolumn(0,0x3c);
+		matrixled_showcolumn(1,0x42);
+		matrixled_showcolumn(2,0xa9);
+		matrixled_showcolumn(3,0x85);
+		matrixled_showcolumn(4,0x85);
+		matrixled_showcolumn(5,0xa9);
+		matrixled_showcolumn(6,0x42);
+		matrixled_showcolumn(7,0x3c);
 	}
 }
