3f7405f6e11aaa02e891c035098a061cc40df556
huangshun <1692470435@qq.com>
2023-11-12 07:45:10 +0800

    中断：定时器

--- STM32/6-1 定时器定时中断/System/Timer.c
+++ STM32/6-1 定时器定时中断/System/Timer.c
@@ -12,18 +12,30 @@ void Timer_Init(void)
 	TIM_TimeBaseInitStruct.TIM_Period = 10000-1;
 	TIM_TimeBaseInitStruct.TIM_Prescaler  = 7200-1;
 	TIM_TimeBaseInitStruct.TIM_RepetitionCounter = 0;
+	TIM_TimeBaseInit(TIM2,&TIM_TimeBaseInitStruct);//初始化时基单元,马上更新
+													//事件使缓存分频寄存器生效
 	
-	TIM_TimeBaseInit(TIM2,&TIM_TimeBaseInitStruct);//初始化时基单元
-	
+	TIM_ClearFlag(TIM2,TIM_IT_Update);//更新事件更新了中断标志位，所以此处置0
 	TIM_ITConfig(TIM2,TIM_IT_Update,ENABLE);//开启更新中断到NVIC的通路
 	
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
 	
 	NVIC_InitTypeDef NVIC_InitStructure;
-	NVIC_InitStructure.NVIC_IRQChannel = ;
-	NVIC_InitStructure.NVIC_IRQChannelCmd = ;
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = ;
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority = ;
-	
+	NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 2;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
 	NVIC_Init(&NVIC_InitStructure);
+	
+	TIM_Cmd(TIM2,ENABLE);
 }
+
+//模板
+//void TIM2_IRQHandler(void)
+//{
+//	if(TIM_GetITStatus(TIM2,TIM_IT_Update) == SET)
+//	{
+//		
+//		TIM_ClearITPendingBit(TIM2,TIM_IT_Update);
+//	}
+//}
--- STM32/6-1 定时器定时中断/User/main.c
+++ STM32/6-1 定时器定时中断/User/main.c
@@ -3,14 +3,25 @@
 #include "OLED.h"
 #include "Timer.h"
 
+uint16_t num;
 int main(void)
 {
 	OLED_Init();
-	OLED_ShowString(1,3,"Hello World");
+	Timer_Init(); 
+	OLED_ShowString(1,1,"Numer:");
 	//OLED_Clear();
 	while(1)
 	{
-	
+		OLED_ShowNum(1,7,num,5);
 		
+	}
+}
+
+void TIM2_IRQHandler(void)
+{
+	if(TIM_GetITStatus(TIM2,TIM_IT_Update) == SET)
+	{
+		num++;
+		TIM_ClearITPendingBit(TIM2,TIM_IT_Update);
 	}
 }
